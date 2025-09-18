32f6f589ccb5cb634534ff5dddb7a30a6151a07a
android040490 <apalkov040490@gmail.com>
2025-09-18 14:51:11 +0300

    refactor: 💡 bullet collision handler

--- src/core/constants/CollisionHandlers.ts
+++ src/core/constants/CollisionHandlers.ts
@@ -14,31 +14,27 @@ export type CollisionHandler = {
 
 export const COLLISION_HANDLERS: CollisionHandler[] = [
   {
-    entity1Components: [HealthComponent],
-    entity2Components: [BulletComponent],
-    handler: (enemy: Entity, bullet: Entity) => {
-      const { damage } = bullet.getComponent(BulletComponent)!;
-
-      const makeDamage = enemy.getComponent(MakeDamageComponent);
-
-      makeDamage
-        ? (makeDamage.damage += damage)
-        : enemy.addComponent(new MakeDamageComponent(damage));
-    },
-  },
-  {
     entity1Components: [],
     entity2Components: [BulletComponent],
     handler: (hitEntity: Entity, bullet: Entity) => {
       const physicsComponent = hitEntity.getComponent(PhysicsComponent);
-      const lifeTime = bullet.getComponent(LifetimeComponent);
+      const bulletLifeTime = bullet.getComponent(LifetimeComponent);
 
       if (
-        lifeTime &&
+        bulletLifeTime &&
         !physicsComponent?.collider?.isSensor() &&
         !hitEntity.hasComponent(BulletComponent)
       ) {
-        lifeTime.timeLeft = 0.05;
+        bulletLifeTime.timeLeft = 0.05;
+      }
+
+      if (hitEntity.hasComponent(HealthComponent)) {
+        const { damage } = bullet.getComponent(BulletComponent)!;
+        const makeDamage = hitEntity.getComponent(MakeDamageComponent);
+
+        makeDamage
+          ? (makeDamage.damage += damage)
+          : hitEntity.addComponent(new MakeDamageComponent(damage));
       }
     },
   },
